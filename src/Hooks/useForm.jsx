3b821b3761d0a8d90b307399b12cd4ebd6d60867
KeziaOliveira <kezia2709@gmail.com>
2024-11-27 19:46:43 -0300

    feat: Login create

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,6 +6,11 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: 'Preencha um email válido',
   },
+  password: {
+    regex: /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W)(?!.* ).{8,16}$/,
+    message:
+      'A senha precisa ter 1 caractere maiúsculo, 1 minúsculo e 1 digito. Mínimo 8 caracteres.',
+  },
 };
 
 const useForm = (type) => {
