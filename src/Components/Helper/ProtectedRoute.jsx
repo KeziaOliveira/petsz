6eb492a68fa58eba7faa68ad55b5befd9c62085b
KeziaOliveira <kezia2709@gmail.com>
2024-11-27 21:23:59 -0300

    feat: Protected route

--- src/Components/Helper/ProtectedRoute.jsx
+++ src/Components/Helper/ProtectedRoute.jsx
@@ -1,3 +1,4 @@
+// Verifica e só da acesso se o usuário estiver logado
 import React from 'react';
 import { UserContext } from '../../UserContext';
 import { Navigate } from 'react-router-dom';
@@ -12,5 +13,10 @@ const ProtectedRoute = ({ children }) => {
     return <></>;
   }
 };
+
+// const ProtectedRoute = ({ children }) => {
+//   const { login } = React.useContext(UserContext);
+//   return login ? children : <Navigate to="/login" />;
+// };
 
 export default ProtectedRoute;
