04c8538960f3c6df2217767d4768ed57a3b1e43e
KeziaOliveira <kezia2709@gmail.com>
2024-12-29 20:45:38 -0300

    feat: Initiated Comment Logic

---
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Enviar from '../../Assets/enviar.svg';
+
+const PhotoCommentsForm = ({ id }) => {
+  const [comment, setComment] = React.useState('');
+
+  return (
+    <form>
+      <textarea
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+    </form>
+  );
+};
+
+export default PhotoCommentsForm;
