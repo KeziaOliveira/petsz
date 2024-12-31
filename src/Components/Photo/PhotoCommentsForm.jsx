c33ca4e03cc39b79039c828e5a2dc02a18c402a9
KeziaOliveira <kezia2709@gmail.com>
2024-12-30 21:17:48 -0300

    feat: Comment Post Logic Finished

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import Enviar from '../../Assets/enviar.svg';
+import Enviar from '../../Assets/enviar.svg?react';
+import { COMMENT_POST } from '../../apis';
+import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
-const PhotoCommentsForm = ({ id }) => {
+const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      // Limpa caixa de comentários após envio
+      setComment('');
+      // Envia o comentário
+      setComments((comments) => [...comments, json]);
+    }
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
+        id="comment"
+        name="comment"
+        // O ideal é uma Label, porém aqui utilizamos apenas o placeholder
+        placeholder="Comente ..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
       <button>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };
