d0b8a370af136d99399e665983680e2ec89b1b0f
KeziaOliveira <kezia2709@gmail.com>
2025-01-11 17:08:09 -0300

    feat: Footer component continued

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import PetszFoot from '../Assets/dogs-footer.svg?react';
 
 function Footer() {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <PetszFoot style={{ margin: '3rem auto 0 auto' }} />
+      <p>Feito com amor por Kezia</p>
+    </footer>
+  );
 }
 
 export default Footer;
