2ebd9c78d54122324957d353a0238ff9a1c9883d
KeziaOliveira <kezia2709@gmail.com>
2025-01-16 19:56:45 -0300

    Tentando dar fix no deploy, agora no vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/',
+//   plugins: [react(), svgr()],
+// });
+
+// Tentando dar fix no deploy
 export default defineConfig({
-  base: '/',
+  base: '/petsz/', // Se não for necessário no Vercel, pode ser removido
+  build: {
+    outDir: 'docs', // Define a pasta de saída
+  },
   plugins: [react(), svgr()],
 });
